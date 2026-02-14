8bc50ae67a25b2962a7078318db269d6c3ca6a0d
DR CHAMPAK MALIVAD <149330753+CSMALIVAD@users.noreply.github.com>
2026-02-15 02:14:37 +0530

    Update renal.js

--- assets/js/renal.js
+++ assets/js/renal.js
@@ -1,3 +1,17 @@
 function analyzeRenal(){
-alert("Renal JS Connected Successfully");
+
+let age = parseFloat(document.getElementById("age").value);
+let creatinine = parseFloat(document.getElementById("creatinine").value);
+
+if(isNaN(age) || isNaN(creatinine)){
+alert("Enter age and creatinine");
+return;
+}
+
+/* Simple CKD-EPI core test calculation */
+let egfr = 141 * Math.pow(creatinine, -1.2) * Math.pow(0.993, age);
+
+document.getElementById("renalOutput").innerHTML =
+"Test eGFR Value: " + egfr.toFixed(1);
+
 }
